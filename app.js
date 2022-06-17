573d0c0b58514ba820c10f61f5289db9844cb517
Rezaul360 <rezaulkarim2448@gmail.com>
2022-06-17 11:48:42 +0600

    update the clock

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ for (let i = 12; i > 0; i--) {
   let option = `<option value="${i}">${i}</option>`;
   selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
 }
-for (let i = 59; i > 0; i--) {
+for (let i = 59; i >= 0; i--) {
   i = i < 10 ? "0" + i : i;
   let option = `<option value="${i}">${i}</option>`;
   selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
